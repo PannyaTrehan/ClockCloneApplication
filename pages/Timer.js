f23b6add0a6c93c7851bbd555b47b498bcc5e0c5
Bishalc4 <s4009432@student.rmit.edu.au>
2024-01-03 22:25:13 +1100

    Timer

--- pages/Timer.js
+++ pages/Timer.js
@@ -8,3 +8,9 @@ export default function Timer() {
     </>
   );
 }
+
+// use state for setting the timer
+// a bool val to know if the timer is running or not
+// reset timer function 
+// formatted in min, second, millisecond --> all formatted in 2 floating point
+// 
